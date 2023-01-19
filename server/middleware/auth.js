6693db054668b6c679c2cbd5bd8718742c015880
Sachin <kumaraditya7125@gmail.com>
2023-01-19 13:51:47 +0530

    added deleted content

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,7 +7,7 @@ const verifyToken = async (req, res, next) => {
     console.log(token);
 
     if (!token) {
-      return res.status(403).json("Access Denied");
+      return res.status(403).json({error:"Access Denied"});
     }
 
     if (token.startsWith("Bearer ")) {
